43eb730c391c055346c2141f4e67dc615c1c405c
Nancy Al Kalach <nancykalach@outlook.com>
2021-01-05 09:25:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+const passport = require('passport');
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
